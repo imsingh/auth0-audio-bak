8341f6c3d271d8b6bc599d9400639211e5a8ed87
Indermohan Singh <indermohansinghk7@gmail.com>
2019-04-04 13:37:36 +0200

    create playback observable

---
+++ src/app/services/audio.service.ts
@@ -0,0 +1,72 @@
+import { Observable, BehaviorSubject, Subject, Observer } from 'rxjs';
+import { takeUntil, share, tap } from 'rxjs/operators';
+import * as moment from 'moment';
+
+export class AudioService {
+  private stop$ = new Subject();
+  private audioObj = new Audio();
+  audioEvents = [
+    'ended', 'error', 'play', 'playing', 'pause', 'timeupdate', 'canplay', 'loadedmetadata', 'loadstart'
+  ];
+
+  playStream(url: string) {
+    return this.streamObservable(url).pipe(
+      takeUntil(this.stop$)
+    );
+  }
+
+  play() {
+    this.audioObj.play();
+  }
+
+  pause() {
+    this.audioObj.pause();
+  }
+
+  stop() {
+    this.stop$.next();
+  }
+
+  seekTo(seconds) {
+    this.audioObj.currentTime = seconds;
+  }
+
+  formatTime(time: number, format: string = 'HH:mm:ss') {
+    const momentTime = time * 1000;
+    return moment.utc(momentTime).format(format);
+  }
+
+  private streamObservable(url) {
+    return Observable.create(observer => {
+      // Play audio
+      this.audioObj.src = url;
+      this.audioObj.load();
+      this.audioObj.play();
+
+      const handler = (event: Event) => {
+        observer.next(event);
+      };
+
+      this.addEvents(this.audioObj, this.audioEvents, handler);
+      return () => {
+        // Stop Playing
+        this.audioObj.pause();
+        this.audioObj.currentTime = 0;
+        // remove event listeners
+        this.removeEvents(this.audioObj, this.audioEvents, handler);
+      };
+    });
+  }
+
+  private addEvents(obj, events, handler) {
+    events.forEach(event => {
+      obj.addEventListener(event, handler);
+    });
+  }
+
+  private removeEvents(obj, events, handler) {
+    events.forEach(event => {
+      obj.removeEventListener(event, handler);
+    });
+  }
+}
