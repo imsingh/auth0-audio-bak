c460c25ca104e9d19b7f2fb90695ec0c0699ff02
Indermohan Singh <indermohansinghk7@gmail.com>
2019-04-04 13:44:11 +0200

    create state management using rxjs

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -1,6 +1,7 @@
 import { Observable, BehaviorSubject, Subject, Observer } from 'rxjs';
 import { takeUntil, share, tap } from 'rxjs/operators';
 import * as moment from 'moment';
+import { StreamState } from '../interfaces/stream-state';
 
 export class AudioService {
   private stop$ = new Subject();
@@ -8,7 +9,16 @@ export class AudioService {
   audioEvents = [
     'ended', 'error', 'play', 'playing', 'pause', 'timeupdate', 'canplay', 'loadedmetadata', 'loadstart'
   ];
-
+  private state: StreamState = {
+    playing: false,
+    readableCurrentTime: '',
+    readableDuration: '',
+    duration: undefined,
+    currentTime: undefined,
+    canplay: false,
+    error: false,
+  };
+  private stateChange: BehaviorSubject<StreamState> = new BehaviorSubject(this.state);
   playStream(url: string) {
     return this.streamObservable(url).pipe(
       takeUntil(this.stop$)
@@ -44,6 +54,7 @@ export class AudioService {
       this.audioObj.play();
 
       const handler = (event: Event) => {
+        this.updateStateEvents(event);
         observer.next(event);
       };
 
@@ -54,6 +65,8 @@ export class AudioService {
         this.audioObj.currentTime = 0;
         // remove event listeners
         this.removeEvents(this.audioObj, this.audioEvents, handler);
+        // reset state
+        this.resetState();
       };
     });
   }
@@ -68,5 +81,42 @@ export class AudioService {
     events.forEach(event => {
       obj.removeEventListener(event, handler);
     });
+  }
+
+  private updateStateEvents(event: Event): void {
+    switch (event.type) {
+      case 'canplay':
+        this.state.duration = this.audioObj.duration;
+        this.state.readableDuration = this.formatTime(this.state.duration);
+        this.state.canplay = true;
+        break;
+      case 'playing':
+        this.state.playing = true;
+        break;
+      case 'pause':
+        this.state.playing = false;
+        break;
+      case 'timeupdate':
+        this.state.currentTime = this.audioObj.currentTime;
+        this.state.readableCurrentTime = this.formatTime(this.state.currentTime);
+        break;
+      case 'error':
+        this.resetState();
+        this.state.error = true;
+        break;
+    }
+    this.stateChange.next(this.state);
+  }
+
+  private resetState() {
+    this.state = {
+      playing: false,
+      readableCurrentTime: '',
+      readableDuration: '',
+      duration: undefined,
+      currentTime: undefined,
+      canplay: false,
+      error: false
+    };
   }
 }
