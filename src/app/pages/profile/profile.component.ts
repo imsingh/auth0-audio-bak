d94898f994cb4390d83ccb81e10b045113b0b587
Indermohan Singh <indermohansinghk7@gmail.com>
2019-04-09 17:41:41 +0200

    create profile and player page

---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+  userProfile;
+  constructor(authService: AuthService) {
+    authService.userProfile$.subscribe(profile => {
+      this.userProfile = profile;
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
