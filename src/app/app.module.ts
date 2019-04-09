83ec4bfee574e7626f620bf257e75f0d0e67077c
Indermohan Singh <indermohansinghk7@gmail.com>
2019-04-09 17:43:11 +0200

    update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// components
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { PlayerComponent } from './pages/player/player.component';
+
+// modules
 import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
 
 // all services
 import { AudioService } from './services/audio.service';
 import { CloudService} from './services/cloud.service';
 import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [
     AudioService,
     CloudService,
-    AuthService
+    AuthService,
+    AuthGuard
+  ],
+  entryComponents: [
+    PlayerComponent,
+    ProfileComponent
   ],
   bootstrap: [AppComponent]
 })
