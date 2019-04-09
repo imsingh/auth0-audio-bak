db4e16d851740c8736a0c4080dc67e4b023e1efd
Indermohan Singh <indermohansinghk7@gmail.com>
2019-04-09 17:42:25 +0200

    user environment files for config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  auth0: {
+    clientID: 'fpgeZTWjHn8Yb5rC5FjmdfNl63yl5KK9',
+    domain: 'angular-auth0-app.eu.auth0.com',
+    redirectUri: 'http://localhost:4200/',
+    logoutUrl: 'http://localhost:4200/'
+  }
 };
 
 /*
